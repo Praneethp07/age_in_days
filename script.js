3f5865fa0d34fba016ffd530e775ce6119b23b08
praneeth <praneethp.cs20@sahyadri.edu.in>
2022-01-23 22:38:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function ageindays()
+{
+    var year = 0;
+    var date =0;
+    var month = 0;
+     year = prompt("ENTER YOUR YEAR OF BIRTH:");
+     date = prompt("ENTER YOUR DATE OF BIRTH:");
+     month = prompt("ENTER MONTH:")
+     if(year == 0)
+     { 
+        var h1 = document.createElement('h1');
+        var ans = document.createTextNode("You are dead");
+        h1.setAttribute('id',ageindays);
+        h1.appendChild(ans);
+        document.getElementById('flex-box-result').appendChild(h1);
+     }
+     else
+     {
+    var birth = document.getElementById('year');
+    var days = (2022 - birth)*365;
+    var h1 = document.createElement('h1');
+    var ans = document.createTextNode("you are "+days+" days old!");
+    h1.setAttribute('id',ageindays);
+    h1.appendChild(ans);
+    document.getElementById('flex-box-result').appendChild(h1);
+     }
+}
+
+function reset()
+{
+    document.getElementById(ageindays).remove();
+    
+}
